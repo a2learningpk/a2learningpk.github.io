2e9594b6628914a3516e128e5476307338f1de7b
a2learningpk <a2learningpk@gmail.com>
2026-05-10 01:45:46 +0500

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,40 @@
+// SET LAUNCH DATE
+const launchDate = new Date("May 13, 2026 00:00:00").getTime();
+
+const countdown = setInterval(() => {
+
+  const now = new Date().getTime();
+
+  const distance = launchDate - now;
+
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+
+
+  const hours = Math.floor(
+    (distance % (1000 * 60 * 60 * 24))
+    / (1000 * 60 * 60)
+  );
+
+  const minutes = Math.floor(
+    (distance % (1000 * 60 * 60))
+    / (1000 * 60)
+  );
+
+  const seconds = Math.floor(
+    (distance % (1000 * 60))
+    / 1000
+  );
+
+  document.getElementById("days").innerHTML = days;
+  document.getElementById("hours").innerHTML = hours;
+  document.getElementById("minutes").innerHTML = minutes;
+  document.getElementById("seconds").innerHTML = seconds;
+
+  if (distance < 0) {
+    clearInterval(countdown);
+
+    document.querySelector(".countdown-container").innerHTML =
+      "<h3>We Are Live Now!</h3>";
+  }
+
+}, 1000);
