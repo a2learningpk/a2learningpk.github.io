4d9d7c8f747076cd832f0d8ae8e05e056463a532
a2learningpk <a2learningpk@gmail.com>
2026-05-18 10:46:41 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 // SET LAUNCH DATE
-const launchDate = new Date("May 13, 2026 00:00:00").getTime();
+const launchDate = new Date("June 1, 2026 00:00:00").getTime();
 
 const countdown = setInterval(() => {
 
